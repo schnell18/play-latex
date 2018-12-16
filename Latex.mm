45294dc4d96b63b9cf046222831e04220573ce69
Justin Zhang <schnell18@gmail.com>
2018-12-16 21:51:21 +0800

    Add math symbols

---
+++ Latex.mm
@@ -0,0 +1,92 @@
+<map version="freeplane 1.6.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="Latex" FOLDED="false" ID="ID_1393233048" CREATED="1544926703210" MODIFIED="1544926710474" STYLE="oval">
+<font SIZE="18"/>
+<hook NAME="MapStyle">
+    <properties edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" fit_to_viewport="false"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24.0 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ICON_SIZE="12.0 pt" COLOR="#000000" STYLE="fork">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffffff" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000" STYLE="oval" SHAPE_HORIZONTAL_MARGIN="10.0 pt" SHAPE_VERTICAL_MARGIN="10.0 pt">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11"/>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="2" RULE="ON_BRANCH_CREATION"/>
+<node TEXT="history" POSITION="right" ID="ID_1039848762" CREATED="1544926711942" MODIFIED="1544926733076">
+<edge COLOR="#ff0000"/>
+<node TEXT="Donald" ID="ID_1039230591" CREATED="1544926734090" MODIFIED="1544926758780"/>
+<node TEXT="Leslie Lamport" ID="ID_486479467" CREATED="1544926759425" MODIFIED="1544926768947"/>
+</node>
+<node TEXT="document layout" POSITION="left" ID="ID_1282360929" CREATED="1544926771105" MODIFIED="1544926778794">
+<edge COLOR="#0000ff"/>
+<node TEXT="structure command" ID="ID_139801584" CREATED="1544926813117" MODIFIED="1544926825239">
+<node TEXT="part" ID="ID_68379502" CREATED="1544926780809" MODIFIED="1544926802271"/>
+<node TEXT="chapter" ID="ID_1465387627" CREATED="1544926802709" MODIFIED="1544926806431"/>
+<node TEXT="section" ID="ID_1986596125" CREATED="1544926868780" MODIFIED="1544926873015"/>
+<node TEXT="subsection" ID="ID_1371060581" CREATED="1544926873900" MODIFIED="1544926877158"/>
+<node TEXT="subsubsection" ID="ID_798753899" CREATED="1544926877900" MODIFIED="1544926881622"/>
+<node TEXT="paragraph" ID="ID_805612160" CREATED="1544926888860" MODIFIED="1544926892789"/>
+<node TEXT="subparagraph" ID="ID_1130539184" CREATED="1544926893475" MODIFIED="1544926899197"/>
+<node TEXT="appendix" ID="ID_962349772" CREATED="1544927229372" MODIFIED="1544927233461"/>
+<node TEXT="general form: \sec_command[short title]{title}" ID="ID_170595270" CREATED="1544926929159" MODIFIED="1544926957391"/>
+</node>
+<node TEXT="document class" ID="ID_1305217878" CREATED="1544926825916" MODIFIED="1544926836614">
+<node TEXT="article" ID="ID_813761898" CREATED="1544926837572" MODIFIED="1544926842133"/>
+<node TEXT="report" ID="ID_1314690111" CREATED="1544926842700" MODIFIED="1544926846014"/>
+<node TEXT="book" ID="ID_1763464121" CREATED="1544926846571" MODIFIED="1544926848205"/>
+</node>
+</node>
+</node>
+</map>
---
+++ README.md
@@ -0,0 +1,3 @@
+# Introduction
+
+This is a repository of Latex studying exercises.
